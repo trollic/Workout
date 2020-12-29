e6b082273b596cec09923b549f1d1d63c6bd2e8a
trollic <cece.jzhu@gmail.com>
2020-12-29 12:05:41 +1100

    Initial Commit

---
+++ Workout/WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutApp.swift
+//  Workout
+//
+//  Created by Cece Zhu on 29/12/20.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
